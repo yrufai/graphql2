52e912048783137c15f84f9e823ae6296d00c400
yakubu rufai mahatma <yrufai23@gmail.com>
2019-10-24 02:17:57 +0000

    post type added

--- src/index.js
+++ src/index.js
@@ -2,32 +2,48 @@ import { GraphQLServer } from "graphql-yoga";
 
 const typeDefs = `
     type Query {
-        title: String!
-        price: Float!
-        releaseYear: Int
-        rating: Float
-        inStock: Boolean!
+      greeting(name: String!, status: String!): String!
+        me: User!
+        post: Post!
+    }
+    type User{
+      id:ID!
+      name: String!
+      email: String!
+      age: Int
+    }
+    type Post{
+      id: ID!
+      title: String!
+      body: String!
+      published: Boolean!
     }
 
-     
 `;
 
 const resolvers = {
   Query: {
-    title() {
-      return "the War of Art";
+    greeting(parent, args, ctx, info) {
+      if (args.name) {
+        return `Hello, ${args.name}, you are my dhopest ${args.status}`;
+      } else {
+        return "Hello";
+      }
     },
-    price() {
-      return 12.99;
+    me() {
+      return {
+        id: "52545",
+        name: "mr snaps",
+        email: "yrufai23@gmail.com"
+      };
     },
-    releaseYear() {
-      return null;
-    },
-    rating() {
-      return 5;
-    },
-    inStock() {
-      return true;
+    post() {
+      return {
+        id: "987654321",
+        title: "the Game of Thrones",
+        body: "hey guys i hope all the kings and queens are doing well",
+        published: false
+      };
     }
   }
 };
